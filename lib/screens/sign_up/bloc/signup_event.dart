a669bddf63fe32187dd126ee8d41a1e40c0e0694
Aqib-Malik <hmaqib92@gmail.com>
2022-08-29 20:20:20 -0700

    first commit

---
+++ lib/screens/sign_up/bloc/signup_event.dart
@@ -0,0 +1,12 @@
+part of 'signup_bloc.dart';
+
+@immutable
+abstract class SignupEvent {}
+
+class OnTextChangedEvent extends SignupEvent {}
+
+class SignUpTappedEvent extends SignupEvent {
+  
+}
+
+class SignInTappedEvent extends SignupEvent {}
